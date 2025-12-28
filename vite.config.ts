2bb06c1fc98011fe839e310b3c62073aa6072399
Ben Ollomo <benji.ollomo@gmail.com>
2025-12-28 00:57:25 -0500

    Add environment variable loading and path aliasing in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,26 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { fileURLToPath, URL } from 'node:url'
 
-export default defineConfig({
-    plugins: [
-        tsconfigPaths(),
-        tailwindcss(),
-        reactRouter(),
-    ],
+
+export default defineConfig(({ command, mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+
+    return {
+        plugins: [
+            tsconfigPaths(),
+            tailwindcss(),
+            reactRouter(),
+        ],
+        define: {
+            __APP_ENV__: JSON.stringify(env.APP_ENV),
+        },
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('./app', import.meta.url))
+            }
+        }
+    }
 });
